bc55edd61f604ab25d8a5fd08e6cf1e025423bb0
olgaIsakov <olgaisakov821997?>
2021-05-09 16:22:30 +0300

    h

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+import { TmpComponent } from './tmp/tmp.component';
+import {MenuModule} from 'primeng/menu';
+import {MenuItem} from 'primeng/api';
+import {ButtonModule} from 'primeng/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainMenuComponent,
+    TmpComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MenuModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/tmp/tmp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tmp',
+  templateUrl: './tmp.component.html',
+  styleUrls: ['./tmp.component.css']
+})
+export class TmpComponent implements OnInit {
+  title = 'web-app';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
