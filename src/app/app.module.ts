0dd6868ceca9b4e242dc972180209d79c39d6f9f
olgaIsakov <olgaisakov821997?>
2021-05-12 23:32:17 +0300

    olga-text-box

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {EditorModule} from 'primeng/editor';
+import {FormsModule} from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainMenuComponent } from './main-menu/main-menu.component';
@@ -8,8 +9,10 @@ import { TmpComponent } from './tmp/tmp.component';
 import {MenuModule} from 'primeng/menu';
 import {MenuItem} from 'primeng/api';
 import {ButtonModule} from 'primeng/button';
+import {TableModule} from 'primeng/table';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+
 
 @NgModule({
   declarations: [
@@ -17,13 +20,16 @@ import { SignUpComponent } from './sign-up/sign-up.component';
     MainMenuComponent,
     TmpComponent,
     SignInComponent,
-    SignUpComponent
-  ],
+    SignUpComponent,
+    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MenuModule,
-    ButtonModule
+    ButtonModule,
+    EditorModule,
+    FormsModule,
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
