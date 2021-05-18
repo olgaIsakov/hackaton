c18fc304875052db7570d5275c3549995f700d1f
AmossDvir <amoss.dvir@mail.huji.ac.il>
2021-05-19 01:25:12 +0300

    fixed menu

--- src/app/main-menu/main-menu.component.ts
+++ src/app/main-menu/main-menu.component.ts
@@ -10,6 +10,7 @@ import { Router} from '@angular/router';
 })
 export class MainMenuComponent implements OnInit {
   items: MenuItem[]=[];
+  activeItem!: MenuItem;
 
 
   constructor(private router: Router) { }
@@ -23,10 +24,30 @@ export class MainMenuComponent implements OnInit {
         {label: 'Home' ,id: 'home', icon: 'pi pi-fw pi-home', routerLink :['/Home']},
         {label: 'Sign In',id: 'signIn', icon: 'pi pi-sign-in', routerLink :['/SignIn']},
         {label: 'Sign Up', id: 'signUp',icon: 'pi pi-user-plus', routerLink :['/SignUp']},
-        {label: 'Add Post', id: 'addPost',icon: 'pi pi-plus-circle', routerLink :['/AddPost']}
-        // {label:'<span class="p-input-icon-right"><i class="pi pi-search"></i><input type="text" pInputText [(ngModel)]="searchValue" /></span>', escape: false
-      // }
+        {label: 'Add Post',items:[{label: 'Home' ,id: 'home', icon: 'pi pi-fw pi-home', routerLink :['/Home']}], id: 'addPost',icon: 'pi pi-plus-circle', routerLink :['/AddPost']},
+        {label:'<span class="p-input-icon-right" width="200"><i class="pi pi-search" ></i><input type="text" pInputText [(ngModel)]="searchValue"/></span>', escape: false
+      }
     ];
+    // alert(location.pathname);
+    // let index = this.items.findIndex(x => x.label === location.pathname);
+    let index = 0;
+    for (let item of this.items){
+      let name = location.pathname;
+      console.log(name+ "   " + item.routerLink);
+      if (item.routerLink == name){
+        break;
+      }
+      index++;
+    }
+    console.log("index is: "+index)
+    let curr = location.pathname;
+    // var index = this.items.findIndex(item => item.label == curr);
+
+      // let search = (this.items, curr) => { return this.items.indexOf(curr); }
+    // alert(index);
+
+    this.activeItem = this.items[index];
+
  }
 
 }
