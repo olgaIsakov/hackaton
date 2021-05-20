57b42d1a10ada9bade84a0830a58e51fb82acc6a
omritamam <65440950+omritamam@users.noreply.github.com>
2021-05-20 15:07:20 +0300

    Revert "Revert "Merge branch 'Omri' into main""
    This reverts commit f0872c2b743b2f19cf055b53a410255430aa825a.

--- src/app/submit-post/submit-post.component.ts
+++ src/app/submit-post/submit-post.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {CommentClass} from "src/db/Classes/CommentClass"
+import {Post} from "src/db/Classes/PostClass"
+import {User} from "src/db/Classes/User"
+import {createComment, createPost, signup, login, getPostByPID} from "src/db/Classes/Api"
+import { post } from 'jquery';
 
 @Component({
   selector: 'app-submit-post',
@@ -12,7 +17,18 @@ export class SubmitPostComponent implements OnInit {
 
    }
 
-   submit(){
+   async submit(){
+     // tests for createPost, createComment, signup
+      var new_user = new User("HOLY","FUCK","its@works.com",false)
+      var new_post = new Post('OH YEAH ITS WOEKING', 5, 23,5)
+      var new_comment = new CommentClass(105,"I LOVE IT", 25,5,10)
+      if(false){
+        createPost(new_post)
+        createComment(new_comment)
+        signup(new_user.username,new_user.password)
+        console.log("return value in submit func is: "+await login(new_user.username,new_user.password))
+     }
+     console.log(getPostByPID(0))
     this.text="this works!!"
   }
 
--- src/db/Classes/User.ts
+++ src/db/Classes/User.ts
@@ -1,4 +1,4 @@
-class User{
+export class User{
     username!: string
     password!: string
     email!: string
