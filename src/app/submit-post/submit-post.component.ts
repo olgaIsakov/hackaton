cb1cbd11705de5c119df3e7d4ce9d4793e900c59
olgaIsakov <77117707+olgaIsakov@users.noreply.github.com>
2021-05-20 12:09:22 +0300

    kkk

--- src/app/submit-post/submit-post.component.ts
+++ src/app/submit-post/submit-post.component.ts
@@ -7,9 +7,12 @@ import {InputSwitchModule} from 'primeng/inputswitch';
   styleUrls: ['submit-post.component.css']
 })
 export class SubmitPostComponent implements OnInit {
-
   text="";
+<<<<<<< HEAD
+
+=======
   isChecked =true;
+>>>>>>> Olga
   constructor() {
 
    }
