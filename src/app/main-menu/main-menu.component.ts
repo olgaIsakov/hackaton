d0675314c7a37735579f1f9a873121cd08efbff4
AmossDvir <amoss.dvir@mail.huji.ac.il>
2021-05-20 23:47:28 +0300

    Logo, sign up fix

--- src/app/main-menu/main-menu.component.ts
+++ src/app/main-menu/main-menu.component.ts
@@ -28,19 +28,33 @@ export class MainMenuComponent implements OnInit {
     //todo: clear cookies
   }
 
+  collapseSearch(){
+    let elem = document.getElementById('collapseExample');
+    console.log(elem?.hidden)
+    if (elem != null){
+      if (elem.hidden === true){
+        elem.hidden=false
+
+      }
+      else {
+        elem.hidden = true
+      }
+    }
+  }
+
 
   ngOnInit() {
 
     // Initialize menu items:
     this.items =[
+        // {icon:'<img src="../../assets/Logo.png" width="70" height="70">',escape:false},
         {icon:'pi pi-bars', command: ()=>{this.display = true}},
         {label: 'Home' ,id: 'home', icon: 'pi pi-fw pi-home', routerLink :['/Home']},
         {label: 'Sign In',id: 'signIn', icon: 'pi pi-sign-in', routerLink :['/SignIn']},
         {label: 'Sign Up', id: 'signUp',icon: 'pi pi-user-plus', routerLink :['/SignUp']},
         {label: 'Add Post', id: 'addPost',icon: 'pi pi-plus-circle', routerLink :['/AddPost']},
-        {label:'Sign Out' ,icon:'pi pi-sign-out',command: this.signOut,style:{'position':'absolute',  'right': '0px'}},
-        {icon:'pi pi-search' ,command: this.sendEmail,style:{'position':'absolute',  'right': '160px'}
-      }
+        {label:'Sign Out' ,icon:'pi pi-sign-out', command: this.signOut, style:{'position':'absolute',  'right': '0px'}},
+        {icon:'pi pi-search' ,command: this.collapseSearch, style:{'position':'absolute',  'right': '160px'}},
     ];
 
     // Find the current page and highlight it on the menu when refreshing:
